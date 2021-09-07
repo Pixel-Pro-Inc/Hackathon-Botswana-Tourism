e313bb53e415bea478b52cd2aa0162a5a92bff82
Abel Tshimbalanga <apexmachine2@gmail.com>
2021-09-07 11:39:46 +0200

    Downloaded and imported firebase.database unity sdk

---
+++ Assets/Scripts/Entities/BProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Assets.Scripts.Helpers.Enums;
+
+namespace Assets.Scripts.Entities
+{
+    class BProfile
+    {
+        public string businessSector { get; set; }
+        public TourismSector sector { get; set; }
+    }
+}
---
+++ Assets/Scripts/Entities/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Entities
+{
+    class Users
+    {
+        public Users Instance { get; set; }
+        public Users()
+        {
+
+        }
+
+        public string username { get; set; }
+        public string useremail { get; set; }
+        public string userpassword { get; set; }
+    }
+}
