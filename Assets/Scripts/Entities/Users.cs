4d285d9c042d1fb32d93825817229e5741c7e97d
Abel Tshimbalanga <apexmachine2@gmail.com>
2021-09-07 12:35:54 +0200

    added writenewUser and updateexistingUser. There are not special though so dont expect anything from them

--- Assets/Scripts/Entities/Users.cs
+++ Assets/Scripts/Entities/Users.cs
@@ -11,9 +11,13 @@ namespace Assets.Scripts.Entities
         public Users Instance { get; set; }
         public Users()
         {
-
         }
 
+        public Users(string username, string email)
+        {
+            this.username = username;
+            this.useremail = email;
+        }
         public string username { get; set; }
         public string useremail { get; set; }
         public string userpassword { get; set; }
--- Assets/Scripts/Helpers/FireBaseHelper.cs
+++ Assets/Scripts/Helpers/FireBaseHelper.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Entities;
+using Firebase.Database;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FireBaseHelper : MonoBehaviour
 {
     // Start is called before the first frame update
+    DatabaseReference reference;
     void Start()
     {
-        
+        #region Google play services version confirmaation
+
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+        #endregion
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void writeNewUser(string userId, string name, string email)
+    {
+        Users user = new Users(name, email);
+        string json = JsonUtility.ToJson(user);
+
+        reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+    }
+    private void updateExistingUser(string userId, string record, string detail)
+    {
+        reference.Child("users").Child(userId).Child(record).SetValueAsync(detail);
     }
 }
